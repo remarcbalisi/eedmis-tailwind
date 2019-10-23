ae26b88a888235824943d667d17a91ee8fa29a6d
Remarc Balisi <remarc.balisi@gmail.com>
2019-10-24 01:49:20 +0800

    create seeder for admin user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class UserTableSeeder extends Seeder
 {
@@ -11,6 +14,22 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $admin_role = Role::create(['name' => 'admin']);
+        $tenant_role = Role::create(['name' => 'tenant']);
+
+        $admin_permission = Permission::create(['name' => 'edit all']);
+        $tenant_permission = Permission::create(['name' => 'manage market stores']);
+
+        $admin_role->givePermissionTo($admin_permission);
+        $tenant_role->givePermissionTo($tenant_permission);
+
+        $admin_user = User::create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'johndoe@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        $admin_user->givePermissionTo($admin_permission);
     }
 }
