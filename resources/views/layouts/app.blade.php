47f7afd58be1927b2c2d22682a2280499443e5fa
Remarc Balisi <remarc.balisi@gmail.com>
2019-10-24 03:21:59 +0800

    admin login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,9 @@
                     </a>
                 </div>
                 <div>
-                    <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a>
+                    @if( !auth()->user() )
+                    <a href="{{route('login')}}" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a>
+                    @endif
                 </div>
             </div>
         </nav>
